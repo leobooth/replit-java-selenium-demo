e54a0ee40338b2c3d1271ff331139e595c3aba49
Leo Booth <leobooth@users.noreply.github.com>
2022-04-21 20:35:45 +0000

    pointed Selenium to practice page, created practice questions

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner; 
+import java.util.List;
+import java.util.ArrayList;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -21,20 +23,49 @@ class Main {
 	
     ChromeDriver driver = new ChromeDriver(options);
     driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-    
-    driver.get("https://google.com");    
-    System.out.println("page title: " + driver.getTitle()); // => "Google"
-    
-    WebElement searchBox = driver.findElement(By.name("q"));
-    WebElement searchButton = driver.findElement(By.name("btnK"));
-    
-    searchBox.sendKeys("Selenium");
-    searchButton.click();
-    
-    searchBox = driver.findElement(By.name("q"));
-    String searchBoxValue = searchBox.getAttribute("value"); // => "Selenium"
-    System.out.println("search box value: " + searchBoxValue);
 
+    /* point Selenium driver to the webpage at address 
+     * "https://seleniumpracticepages.leobooth.repl.co/"
+    */
+    driver.get("https://seleniumpracticepages.leobooth.repl.co/");
+
+    // print page title to console
+    System.out.println("page title: " + driver.getTitle());
+
+    // print the text in the second 'target' element to the console
+    WebElement target2 = driver.findElement(
+      By.xpath("//div[contains(text(),'target')][2]")
+    );
+    System.out.println("second 'target' element: " + target2.getText());
+
+    /* use a single selector to get both the first and second 'target' elements
+     * then print the text from both elements to the console
+    */
+    List<WebElement> targets = driver.findElements(
+      By.xpath("//div[contains(text(),'target')][position()<=2]")
+    );
+    
+    int targetCounter = 0;
+    for(WebElement target : targets) {
+          targetCounter++;
+          System.out.println("'target' element " + targetCounter + " text:" +  
+          target.getText());
+    }
+
+    /* select the "div" elements that contain numbers only
+     * then print the text from all the elements to the console
+    */
+    List<WebElement> divs = driver.findElements(
+      By.xpath("//div[not(contains(text(),'target'))]")
+    );
+    
+    int divCounter = 0;
+    for(WebElement div : divs) {
+          divCounter++;
+          System.out.println("'div' element " + divCounter + " text:" +  
+          div.getText());
+    }
+    
     /* use this Scanner section to pause the test
      * until you are ready to close the web browser
     */ 
